b5ca8f1d3de21da958a2506195d94180aea37023
Shinichi Haramizu <shinichi@haramizu.jp>
2024-06-25 17:02:23 +0900

    update themes

---
+++ .storybook/XmcTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'XM Cloud Components - Sample',
+  brandUrl: 'https://xmc-components.vercel.app/',
+  brandImage: 'https://delivery-sitecore.sitecorecontenthub.cloud/api/public/content/logo-components-dark',
+  brandTarget: '_self',
+});
