494cc7be232da3f327e6d4f98927cec21b9ca335
Shinichi Haramizu <shinichi@haramizu.jp>
2024-06-28 16:53:31 +0900

    add sample data

--- src/stories/Navigation.tsx
+++ src/stories/Navigation.tsx
@@ -1,48 +1,143 @@
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  Transition,
+} from "@headlessui/react";
+import Link from "next/link";
 import React, { useState } from "react";
+
+interface Fields {
+  Id: string;
+  DisplayName: string;
+  Title: string;
+  NavigationTitle: string;
+  Href: string;
+  Querystring: string;
+  Styles: string[];
+  Children: Array<Fields>;
+}
 
 type NavigationProps = {
   params: { [key: string]: string };
+  fields: Fields;
 };
 
 export const Default = (props: NavigationProps): JSX.Element => {
-  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsSubMenuOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsSubMenuOpen(false);
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={`component navigation ${props.params.styles.trimEnd()}`}>
-      <div className="relative">
-        <button
-          className="block px-4 py-2 text-gray-800 hover:bg-gray-200 focus:outline-none"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          メニュー
-        </button>
-        {isSubMenuOpen && (
-          <div className="absolute left-0 mt-2 py-2 w-48 bg-white border border-gray-200 shadow-lg rounded-lg">
-            <a
-              href="#"
-              className="block px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900"
-            >
-              サブメニュー1
-            </a>
-            <a
-              href="#"
-              className="block px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-gray-900"
-            >
-              サブメニュー2
-            </a>
-            {/* 他のサブメニュー項目も同様に追加 */}
-          </div>
-        )}
+      <div className="mx-2">
+        <a href="#">Home</a>
+      </div>
+      <ProductList />
+      <SolutionList />
+      <div className="mx-2">
+        <a href="#">About</a>
       </div>
     </div>
+  );
+};
+
+export const SolutionList = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Popover className="relative">
+        {() => (
+          <>
+            <PopoverButton
+              onMouseEnter={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
+              className={`navigation-title mx-2 font-semibold`}
+              key={2}
+            >
+              <Link href="#">Solutions</Link>
+            </PopoverButton>
+            <Transition
+              show={isOpen}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <PopoverPanel
+                onMouseEnter={() => setIsOpen(true)}
+                onMouseLeave={() => setIsOpen(false)}
+                className="absolute mt-1 w-screen max-w-[280px] transform bg-white z-50"
+              >
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
+                  <div className="p-2">
+                    <ul>
+                      <li>
+                        <a href="#">Solution 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Solution 2</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </PopoverPanel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </>
+  );
+};
+
+export const ProductList = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Popover className="relative">
+        {() => (
+          <>
+            <PopoverButton
+              onMouseEnter={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
+              className={`navigation-title mx-2 font-semibold`}
+              key={2}
+            >
+              <Link href="#">Products</Link>
+            </PopoverButton>
+            <Transition
+              show={isOpen}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <PopoverPanel
+                onMouseEnter={() => setIsOpen(true)}
+                onMouseLeave={() => setIsOpen(false)}
+                className="absolute mt-1 w-screen max-w-[280px] transform bg-white z-50"
+              >
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
+                  <div className="p-2">
+                    <ul>
+                      <li>
+                        <a href="#">Product 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Product 2</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </PopoverPanel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </>
   );
 };
