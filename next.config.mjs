892359b80988f74366971dced065a2d85026964f
Shinichi Haramizu <shinichi@haramizu.jp>
2024-12-04 14:53:34 +0900

    delete export

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import createMDX from "@next/mdx";
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  output: "export",
 };
 
 const withMDX = createMDX({
